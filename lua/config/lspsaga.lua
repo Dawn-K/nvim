cf1d320333f2611de3591f66a3313b49e94a902d
Dawn-K <kon.gzhen@163.com>
2022-10-18 21:19:25 +0800

    remove bulb sign

--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -5,6 +5,15 @@ function M.setup()
 		-- todo fix code_action
 		rename_action_quit = "<C-c>",
 		rename_in_select = false,
+		code_action_lightbulb = {
+			enable = true,
+			enable_in_insert = true,
+			cache_code_action = true,
+			sign = false,
+			update_time = 150,
+			sign_priority = 20,
+			virtual_text = true,
+		},
 	}
 end
 
